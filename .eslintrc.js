b587bc3f558cdd6f0cbf2f6793c3eec0542efaf8
Wilberforce <wmaggickk@gmail.com>
2024-10-26 02:13:16 +0000

    started a ride hailing app using react native and typescript

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier", "import"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  prettierOptions: {
+    endOfLine: "lf",
+  },
+};
